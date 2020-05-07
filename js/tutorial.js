f46fc5377838be39628f9fc375aaa60b144328cf
Haunted Bees <fench@hauntedbees.com>
2020-05-06 18:37:35 -0700

    HTTPS everywhere!

--- js/tutorial.js
+++ js/tutorial.js
@@ -12,7 +12,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 var tutorialStates = [
 	{requiredClick: [3, 4], text: "Hey kid, wanna see a dead body? Tap the sapling the floating hand is pointing at!"}, 
